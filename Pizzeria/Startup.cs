9ab420eae3c4c7de956375e6ea08f1b9e200e388
Dawid <dawid@example.com>
2017-07-22 18:57:41 +0200

    Create roles and add super user

--- Pizzeria/Startup.cs
+++ Pizzeria/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Pizzeria.Data;
 using Pizzeria.Models;
 using Pizzeria.Services;
+using Microsoft.AspNetCore.Identity;
 
 namespace Pizzeria
 {
@@ -55,7 +56,7 @@ namespace Pizzeria
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -83,6 +84,50 @@ namespace Pizzeria
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            await CreateRoles(serviceProvider);
+        }
+
+        /// <summary>
+        /// Source: https://gooroo.io/GoorooTHINK/Article/17333/Custom-user-roles-and-rolebased-authorization-in-ASPNET-core/28352#.WXNv3YjyiUl
+        /// </summary>
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles 
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string[] roleNames = { "Admin", "Employee", "Member" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //Here you could create a super user who will maintain the web app
+            var poweruser = new ApplicationUser
+            {
+                UserName = "DawidKropkaD@wp.pl",
+                Email = "DawidKropkaD@wp.pl",
+            };
+            //Ensure you have these values in your appsettings.json file
+            string userPWD = "TymczasoweHasło.1";
+            var _user = await UserManager.FindByEmailAsync("DawidKropkaD@wp.pl");
+
+            if (_user == null)
+            {
+                var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
+                if (createPowerUser.Succeeded)
+                {
+                    //here we tie the new user to the role
+                    await UserManager.AddToRoleAsync(poweruser, "Admin");
+                }
+            }
         }
     }
 }
